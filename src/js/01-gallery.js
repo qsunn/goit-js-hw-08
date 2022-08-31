4616ec3015e3c36fbbd5f2f66dba83d3c78343b2
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-08-31 20:22:14 +0300

    upg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "../../node_modules/simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+const generateGallery = gallery => {
+    return gallery
+        .map(item => {
+            return `
+            <a class="gallery__item" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                alt="${item.description}"
+                />
+            </a>
+        `
+        })
+        .join('');
+};
+gallery.insertAdjacentHTML('beforeend', generateGallery(galleryItems));
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
